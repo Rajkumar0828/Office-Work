890da88abe9be9b5d643319f417e22a4dd500d9f
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2023-12-06 15:28:15 +0530

    Add files via upload

---
+++ Dependency injection/Constructor Dependency injection/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructorinjection
+{
+                                        
+    public interface ImessageService{   //interface use for representing a service
+        void SendMessage(string message);
+    }
+    public class EmailService : ImessageService   //Concrete implementation of Imessageservice 
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Sending email: {message}");
+        }
+
+
+
+    }
+
+    public class NotificationService   //Class that depends on ImessageService through Constructor injection
+    {
+
+        private readonly ImessageService _messageService;
+
+                  
+        public NotificationService(ImessageService messageService)  //constructor injection
+        {
+            _messageService = messageService;
+        }
+         public void NotifyUser(string username, string message) //method that uses the injected dependecny
+      {
+         string fullMessage =$"Hello, {username}!{message}";    
+         _messageService.SendMessage(fullMessage);
+      }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var  emailService= new EmailService(); // create an instance for the service 
+
+            var notificationservice = new NotificationService(emailService);  // inject the dependency through constructor
+
+            notificationservice.NotifyUser("Rajkumar", "You have a new notification"); //Use the service
+
+            Console.ReadLine();
+        
+        }
+    }
+
+      
+
+    }
+    
+
