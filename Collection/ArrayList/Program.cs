7553f52bc14bfb2292502d4949a9cbac0c043ed4
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2023-12-06 15:25:46 +0530

    Add files via upload

---
+++ Collection/ArrayList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualBasic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var CourseList = new ArrayList();
+
+        CourseList.Add("C#");
+        CourseList.Add("python");
+        CourseList.Add("Java");
+
+
+        var CourseList2 = new ArrayList()
+        {
+
+            1,"Cloud Computing","Devops","Data Engineering"
+        };
+
+        Console.WriteLine("CourseList 1 Elements");
+
+        for(int i = 0;i< CourseList.Count; i++) 
+        Console.WriteLine(CourseList[i]);
+
+        Console.WriteLine("CourseList 2 Elements");
+
+        for(int i = 0;i< CourseList.Count; i++) 
+        Console.WriteLine(CourseList2[i]);
+    }
+
+  
+}
