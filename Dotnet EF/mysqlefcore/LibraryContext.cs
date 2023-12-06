75efa80affbac5bb011a55c2a77474e666af455e
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2023-12-06 15:20:25 +0530

    Add files via upload

---
+++ Dotnet EF/mysqlefcore/LibraryContext.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+using System;
+
+namespace mysqlefcore
+{
+  public class LibraryContext : DbContext
+  {
+    public DbSet<Book> Book { get; set; }
+
+    public DbSet<Publisher> Publisher { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      
+
+        optionsBuilder.UseMySql(
+           "server=localhost;database=library;user=root;password=Password@12345",
+            new MySqlServerVersion(new Version(8, 0, 11))
+        );
+    }
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Publisher>(entity =>
+      {
+        entity.HasKey(e => e.ID);
+        entity.Property(e => e.Name).IsRequired();
+      });
+
+      modelBuilder.Entity<Book>(entity =>
+      {
+        entity.HasKey(e => e.ISBN);
+        entity.Property(e => e.Title).IsRequired();
+        entity.HasOne(d => d.Publisher)
+          .WithMany(p => p.Books);
+      });
+    }
+  }
+}
