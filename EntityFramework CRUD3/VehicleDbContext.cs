6356fe8e453ec4ab7bf6ad6586ffc9ba4243a685
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 11:59:37 +0530

    Add files via upload

---
+++ EntityFramework CRUD3/VehicleDbContext.cs
@@ -0,0 +1,20 @@
+using System.Formats.Tar;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using VehicleManagement.data;
+using VehicleManagementSystem;
+namespace VehicleManagement.data
+{
+
+    public class VehicleDbContext : DbContext
+    {
+        string constr = Program.GetDbConnection();
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(constr,new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+
+}
