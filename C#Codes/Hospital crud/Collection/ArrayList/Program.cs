60839d833311ca4b7b9a1c30fb30d061ad7ee771
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 15:07:39 +0530

    Add files via upload

---
+++ C#Codes/Hospital crud/Collection/ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualBasic;
+
+namespace Arraylist
+{
+public class ArrayProgram
+{
+    public static void Main(string[] args)
+    {
+        var CourseList = new ArrayList();
+
+        CourseList.Add("C#");
+        CourseList.Add("python");
+        CourseList.Add("Java");
+
+
+        var CourseList2 = new ArrayList()
+        {
+
+            1,"Cloud Computing","Devops","Data Engineering"
+        };
+
+        Console.WriteLine("CourseList 1 Elements");
+
+        for(int i = 0;i< CourseList.Count; i++) 
+        Console.WriteLine(CourseList[i]);
+
+        Console.WriteLine("CourseList 2 Elements");
+
+        for(int i = 0;i< CourseList.Count; i++) 
+        Console.WriteLine(CourseList2[i]);
+    }
+
+  
+}
+}
