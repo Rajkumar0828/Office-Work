1ac35eabfd9235d71948121ee3fb3d5dd29295b5
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 14:41:45 +0530

    Add files via upload

---
+++ JenkinsWorks/Testme.cs
@@ -0,0 +1,16 @@
+﻿namespace Jenkinsdemo
+{
+    public class Testme
+    {
+        public string sayJenkins(int times)
+        {
+            string name = "";
+            for  (int i = 0;i<times;i++)
+            {
+                name += "Jen" +
+                    "kins";
+            }
+            return name;
+        }
+    }
+}
