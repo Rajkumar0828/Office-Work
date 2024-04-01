5dd691c260a7f48b61d9a8ab1763fe404beab900
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 15:18:18 +0530

    Add files via upload

---
+++ C#Codes/Hospital crud/C# generic program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CSharpGeneric;
+namespace CSharpGeneric  
+{  
+
+    public abstract class  Course<TSubject>
+    {
+        public abstract void GetMarks(TSubject subject, int marks);
+    }
+
+    public class Student<TSubject> : Course<TSubject>
+    {
+        public override void GetMarks(TSubject subject, int marks)
+        {
+            Console.WriteLine($"Subject: {subject} Marks:{marks}");
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student<string> student1 =new Student<string>();
+        student1.GetMarks("c#", 97);
+         Student<string> student2 =new Student<string>();
+        student2.GetMarks("c#", 77);
+    }
+}
+
+
---
+++ C#Codes/Hospital crud/c# console program set 3 Delegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+public delegate double Student(double mark1, double mark2, double mark3);
+class Marks
+ {    
+    public static double Total(double mark1, double mark2, double mark3)
+    {
+       return (mark1 + mark2 + mark3);    
+       }
+
+   public static double Average(double mark1, double mark2, double mark3)     {
+         return ((mark1 + mark2 + mark3) / 3);
+    }
+
+ }
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {        
+        Student TotalMarks= Marks.Total;
+         Student AverageMarks= Marks.Average;
+
+     Console.WriteLine("Enter the first mark");
+     int mark1=Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Enter the Second mark");
+     int mark2=Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Enter the Third Mark");
+     int mark3=Convert.ToInt32(Console.ReadLine());
+
+     Double result1= PerformCalculation(mark1, mark2, mark3,TotalMarks);
+     Double result2= PerformCalculation(mark1, mark2, mark3,AverageMarks);
+
+     Console.WriteLine($"Ans:{result1}");
+      Console.WriteLine($"Ans:{result2}");
+     }
+     static double PerformCalculation(double mark1, double mark2,double mark3,Student calculation)
+     {
+         return calculation(mark1,mark2,mark3);
+    }
+
+    }
+
+
+ 
