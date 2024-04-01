7682ce59f058bbd1e71e7754251b0135953fd767
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 15:10:33 +0530

    Add files via upload

---
+++ C#Codes/Hospital crud/Dependency injection/Method dependency injection/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodinjection
+{
+                                        
+    public interface ImessageService{   //interface use for representing a service
+        void SendMessage(string message);
+    }
+    public class EmailService : ImessageService   //Concrete implementation of Imessageservice 
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Sending email: {message}");
+        }
+    }
+
+    public class MessageSender   //Class that depends on ImessageService through method injection
+    {
+
+        public void SendNotification(ImessageService messageService,string recipient,string message)
+        {
+            string fullMessage = $"To:{recipient}\nMessage: {message}";
+            messageService.SendMessage(fullMessage);
+
+        }
+    } 
+
+    class Program
+    {
+        static void Main()
+        {
+            var  emailService= new EmailService(); // create an instance for the service 
+
+            var MessageSender = new MessageSender();  // inject the dependency through constructor
+
+            MessageSender.SendNotification(emailService, "rajkumar@relevantz.com","you have new notification!");
+
+
+        
+        }
+    }
+
+      
+
+    }
+    
+
+
+
+
---
+++ C#Codes/Hospital crud/Dependency injection/Property dependency injection/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propertyinjection
+{
+                                        
+    public interface ImessageService{   //interface use for representing a service
+        void SendMessage(string message);
+    }
+    public class EmailService : ImessageService   //Concrete implementation of Imessageservice 
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Sending email: {message}");
+        }
+    }
+    public class WhatsappService : ImessageService   //Concrete implementation of Imessageservice 
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Sending Whatsapp Message: {message}");
+        }
+    }
+
+
+    public class NotificationService   //Class that depends on ImessageService through Constructor injection
+    {
+
+        public ImessageService MessageService { get; set; } // property for dependency injection
+
+
+        public void NotifyUser(string username, string Message) // method that uses the injected dependency
+        {
+            string fullMessage = $"Hello,  {username}! {Message}";
+            MessageService.SendMessage(fullMessage);
+        }
+
+                  
+        
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            EmailService  emailService= new EmailService(); // create an instance for the service 
+            WhatsappService  WhatsappService= new WhatsappService(); 
+
+            NotificationService notificationservice = new NotificationService();  // create instance for service 
+                       // create instance for service 
+
+            notificationservice.MessageService = WhatsappService;
+            // notificationservice.MessageService = emailService;
+
+            notificationservice.NotifyUser("Rajkumar", "You have a new notification"); //Use the service
+
+            Console.ReadLine();
+        
+        }
+    }
+
+      
+
+    }
+    
