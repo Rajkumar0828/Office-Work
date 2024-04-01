3049a23cd2137e6747265b64edebb98160534719
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 15:22:17 +0530

    Add files via upload

---
+++ C#Codes/Hospital crud/Regular expression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regular_expression
+{
+    class Regular_expression_program
+    {
+        static void Main(string[] args)
+        {
+            string courses;
+            courses="C, C++, JAVA, F# are programming languages";
+            Regex read= new Regex(" |. ");
+            StringBuilder space= new StringBuilder();
+            var currentDate=DateTime.Now;
+
+            int count=1;
+            foreach(String sub in  read.Split(courses))
+            {
+                space.AppendFormat("{0} : {1} \n",count++,sub);
+
+            }
+
+            Console.WriteLine(space);
+            Console.WriteLine($" date:  {currentDate}");
+             Console.WriteLine($" Your DOTNET VERSION  :  {Environment.Version}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ C#Codes/Hospital crud/multithreading/Program.cs
@@ -0,0 +1,63 @@
+﻿// using System;
+// using System.Threading;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         // Creating two threads for printing letters and numbers
+//         Thread letterThread = new Thread(PrintLetters);
+//         Thread numberThread = new Thread(PrintNumbers);
+
+//         // Starting both threads
+//         letterThread.Start();
+//         numberThread.Start();
+
+//         // Waiting for both threads to finish
+//         letterThread.Join();
+//         numberThread.Join();
+//      Console.WriteLine("hi");
+//         Console.ReadLine();
+//     }
+
+//     static void PrintLetters()
+//     {
+//         for (char c = 'A'; c <= 'E'; c++)
+//         {
+//             Console.WriteLine(c);
+            
+//              Thread.Sleep(500); // Adding a small delay for better visualization
+//         }
+//     }
+
+//     static void PrintNumbers()
+//     {
+//         for (int i = 1; i <= 5; i++)
+//         {
+//             Console.Write(i);
+//             // Thread.Sleep(1000); // Adding a small delay for better visualization
+//         }
+//     }
+// }
+
+using System;  
+namespace FunctionExample  
+{  
+    class Program  
+    {  
+        // User defin----ed function  
+        public void Show(string message)  
+        {  
+         Console.WriteLine("Inside Show Function"+message);  
+ 
+        }  
+        // Main function, execution entry point of the program  
+        static void Main(string[] args)  
+        {  
+            Program program = new Program();  
+            program.Show("Rahul Kumar");  
+        
+        }  
+    }  
+}  
+
