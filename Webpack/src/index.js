1ec56c611e9d9208faaa298630fa29eda071cd75
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 12:52:10 +0530

    Add files via upload

---
+++ Webpack/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+ReactDOM.render(
+<h1>Welcome Arun and Sneha wait 
+    
+   
+   rendu perukum arivey ilaa
+   hey lusu kala
+   arivey ilaa da ungalaku
+    
+    </h1>,
+   
+
+  document.getElementById('root')
+);
+
