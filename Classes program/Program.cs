8df73ed256b84b66c7afbce0b8b3ce05fbd88c12
R Rajkumar <111265133+Rajkumar0828@users.noreply.github.com>
2024-04-01 15:48:01 +0530

    Add files via upload

---
+++ Classes program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+ namespace CarApplication
+ {
+    class Speed
+    {
+       public  int maxSpeed;
+
+        public  Speed(int max)
+        {
+            maxSpeed=max;
+        }
+    }
+ 
+
+class Car 
+{
+    public string Model;
+    public string color;
+    public Car(string ModelName, string Modelcolor)
+    {
+        Model = ModelName;
+        color = Modelcolor;
+     }
+}
+
+class Program{
+
+    public static void Main(string[] args)
+    {
+        Car Ford = new Car("ford","Red");
+        Speed speed= new Speed(200);
+
+        Console.WriteLine(Ford.Model +"  "+ Ford.color +"  "+ speed.maxSpeed);
+    }
+}
+}
---
+++ Getset method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Carapp
+{
+    class Speed
+    {
+        public int speeding
+        {
+           get; set; 
+        }
+
+    }
+    class Car
+    {
+        
+        public string color { get; set; }
+        public string model { get; set; }
+        
+    }
+
+    class Program : Car
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.model = "Audi";
+            car.color = "Red";
+
+        Speed speed= new Speed();
+            speed.speeding = 250;
+
+        Console.WriteLine(car.model +" "+ car.color +"  "+ speed.speeding);
+        }
+    }
+}
